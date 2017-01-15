fc1219d2396acab1ecfc3d1011253754ede1a012
popokatapepel <jan-seins@hotmail.de>
2017-01-15 17:45:54 +0100

    unittest for analysing track distance

--- app/src/androidTest/java/com/example/heregpsloc/unittest_track.java
+++ app/src/androidTest/java/com/example/heregpsloc/unittest_track.java
@@ -13,22 +13,26 @@ import static org.junit.Assert.assertEquals;
 
 public class unittest_track {
     track trk;
-    double result_climb, result_pos_climb, result_neg_climb;
+    double result_climb, result_pos_climb, result_neg_climb, result_distance;
 
 
     private void init(){
         trk=new track("testrack");
         datapoint dp;
         long time=System.currentTimeMillis();
-        trk.setdatapoint(new datapoint(0,0,100,10.3,time));
+        trk.setdatapoint(new datapoint(47.235909,9.591282,100,10.3,time));
         time=time+1500;
-        trk.setdatapoint(new datapoint(0,0,200,10.3,time));
+        trk.setdatapoint(new datapoint(49.049788, 8.462741,200,10.3,time));
+        //estimated distance to point 1: 218.1km (135.52mi)
         time=time+9000;
-        trk.setdatapoint(new datapoint(0,0,30,10.3,time));
+        trk.setdatapoint(new datapoint(48.779006, 11.405524,30,10.3,time));
+        //estimated distance to point 1: 217.13km (134.92mi)
+        //total estimated distance:
 
         result_climb=-70;
         result_pos_climb=100;
         result_neg_climb=-170;
+        result_distance=218100+217130;
         trk.analyse_track();
     }
 
@@ -47,6 +51,12 @@ public class unittest_track {
     public void test_posclimb(){
         init();
         assertEquals(trk.positive_climb(),result_pos_climb,0);
+    }
+    @Test
+    public void test_distance(){
+        init();
+        assertEquals(trk.distance(),result_distance,result_distance*0.05);
+        //since values are only estimated an accuracy of 5% is sufficient
     }
 
 }
