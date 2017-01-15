592e71fbfa4f128edc73b10be2c437f0437c46dd
popokatapepel <jan-seins@hotmail.de>
2017-01-15 14:06:10 +0100

    calc climb and distance

--- app/src/main/java/com/example/heregpsloc/track.java
+++ app/src/main/java/com/example/heregpsloc/track.java
@@ -15,11 +15,23 @@ import java.util.List;
 public class track {
     //constructor
     public track(String _trackname){trackname=_trackname;}
+    //properties
     private String trackname;
     private List<datapoint> datapoints=new ArrayList<>();
+    private double distance;
+    private double climb;
+    private double positive_climb;
+    private double negative_climb;
+    //setter
     public void setdatapoint(datapoint _dp){datapoints.add(_dp);}
+    //getter
     public List<datapoint> getDatapoints(){return datapoints;}
+    public double distance(){return distance;}
+    public double climb(){return climb;}
+    public double positive_climb(){return positive_climb;}
+    public double negative_climb(){return negative_climb;}
 
+    //calc functions
     private void sortdatapointsbytime(){
     Collections.sort(datapoints, new Comparator<datapoint>() {
         @Override
@@ -28,44 +40,43 @@ public class track {
         }
     });
     }
-
-
-    double distance() {
-        //does not contain altitude
+    private void analyse_date(){
+        initvars();
         sortdatapointsbytime();
-
         boolean init=true;
         double totaldistance=0;
         datapoint dp_prev = null;
         for (datapoint dp:datapoints){
-            if (init){
-                init=false;}
+            if (init) init=false;
             else {
-                Location locationA = new Location("point A");
-
-                locationA.setLatitude(dp_prev.latitude);
-                locationA.setLongitude(dp_prev.longitude);
-
-                Location locationB = new Location("point B");
-
-                locationB.setLatitude(dp.latitude);
-                locationB.setLongitude(dp.longitude);
-
-                totaldistance = totaldistance+locationA.distanceTo(locationB);
+                distance=distance+calc_distance(dp_prev,dp);
+                double current_climb=calc_climb(dp_prev,dp);
+                if (current_climb>0) positive_climb=positive_climb+current_climb;
+                else negative_climb=negative_climb+current_climb;
             }
             dp_prev=dp;
         }
-        return totaldistance;
+        climb=positive_climb+negative_climb;
+    }
+    private void initvars(){
+         distance=0;
+         climb=0;
+         positive_climb=0;
+         negative_climb=0;
     }
 
-    long pos_climb() {
-
-
-        throw new UnsupportedOperationException();
+    private double calc_distance(datapoint dp1, datapoint dp2) {
+        Location locationA = new Location("point A");
+        locationA.setLatitude(dp1.latitude);
+        locationA.setLongitude(dp1.longitude);
+        Location locationB = new Location("point B");
+        locationB.setLatitude(dp2.latitude);
+        locationB.setLongitude(dp2.longitude);
+        return locationA.distanceTo(locationB);
     }
 
-    long neg_climb() {
-        throw new UnsupportedOperationException();
+    private double calc_climb(datapoint dp1, datapoint dp2){
+        return dp2.altitude-dp1.altitude;
     }
 
     protected void send2db() {
